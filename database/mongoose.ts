0c2555c0993f143600fb1d32f423f5109be61a19
Bilelbourgou <bilelbourgou@gmail.com>
2026-03-07 01:41:19 +0100

    feat: Add Mongoose for database connection and implement a cached connection utility.

---
+++ database/mongoose.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error("Please add your MongoDB URI to .env.local");
+}
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    };
+}
+
+let cached = global.mongooseCache || (global.mongooseCache = { conn: null, promise: null });
+
+
+export const connectToDatabase = async () => {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            bufferCommands: false,
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+};
+
+
