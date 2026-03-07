34a98387e7c0ab879c1da1fc2f3247b425b9e094
Bilelbourgou <bilelbourgou@gmail.com>
2026-03-07 01:13:05 +0100

    refactor: Restructure `BookCard` HTML, fix `Navbar` styling, and ignore Clerk configuration files.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     const pathName = usePathname();
     const { user } = useUser();
   return (
-    <header className="w-full fixedz-50 bg-[--bg-primary]">
+    <header className="w-full fixed z-50 bg-(--bg-primary)">
     <div className='wrapper navbar-height py-4 flex justify-between items-center'>
        <Link href="/" className='flex items-center gap-2'>
         <Image src="/logo.png" alt="Logo" width={42} height={26} />
